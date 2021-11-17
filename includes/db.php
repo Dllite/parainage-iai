d2f8e815f01a83f50264040213c65e3690d3d7ed
Dllite <89609595+Dllite@users.noreply.github.com>
2021-11-17 16:41:08 +0100

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,8 @@
+<?php
+$serveur="localhost";
+$username="root";
+$password="";
+$dbname="iai";
+$db=mysqli_connect($serveur, $username, $password, $dbname) or die("erreur de connexion a la base de donnée");
+
+?>
